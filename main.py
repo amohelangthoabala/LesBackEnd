44dc8c8a839c3c18b226be18ae82ab4dbb10c089
maluke lereko <lerekomaduke@gmail.com>
2022-02-20 07:17:07 +0200

    add home url to main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+print("import dependencies...")
 from fastapi import FastAPI
 from interface.routes import users, authentication
 from infrastructure.orm.sqlalchemy import models
 from infrastructure.database.database import engine
 
 app = FastAPI()
+print("app strating...")
 
 models.Base.metadata.create_all(engine)
 #models.Base.metadata.drop_all(engine)
 
+@app.get('/')
+def home():
+    return "api running"
+
 app.include_router(authentication.router)
 app.include_router(users.router)
+
+print("running...")
