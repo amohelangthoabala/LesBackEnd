0d8399d28a27b4eac19b8d49a712bcf12d1e449d
maluke lereko <lerekomaduke@gmail.com>
2022-02-25 18:15:47 +0200

    Add __init__ files

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 print("import dependencies...")
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-# from interface.routes import users, authentication, message, chat, websocket
-# from infrastructure.orm.sqlalchemy import models
-# from infrastructure.database.database import engine
-# from infrastructure.websocket.WebsocketConnectionManager import WebsocketConnectionManager
+from interface.routes import users, authentication, message, chat, websocket
+from infrastructure.orm.sqlalchemy import models
+from infrastructure.database.database import engine
+from infrastructure.websocket.WebsocketConnectionManager import WebsocketConnectionManager
 
 app = FastAPI()
 print("app strating...")
 
-# models.Base.metadata.create_all(engine)
+models.Base.metadata.create_all(engine)
 # #models.Base.metadata.drop_all(engine)
 
 from fastapi.responses import HTMLResponse
@@ -52,29 +52,29 @@ html = """
 
 
 
-# manager = WebsocketConnectionManager()
+manager = WebsocketConnectionManager()
 
 
 @app.get('/')
 async def home():
     return HTMLResponse(html)
 
-# @app.websocket("/websocket/{client_id}")
-# async def websocket_endpoint(websocket: WebSocket, client_id: int):
-#     await manager.connect(websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             await manager.send_personal_message(f"You wrote: {data}", websocket)
-#             await manager.broadcast(f"Client #{client_id} says: {data}")
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket)
-#         await manager.broadcast(f"Client #{client_id} left the chat")
+@app.websocket("/websocket/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
-# app.include_router(authentication.router)
-# app.include_router(users.router)
-# app.include_router(chat.router)
-# app.include_router(message.router)
-# # app.include_router(websocket.router)
+app.include_router(authentication.router)
+app.include_router(users.router)
+app.include_router(chat.router)
+app.include_router(message.router)
+# app.include_router(websocket.router)
 
-# print("running...")
+print("running...")
